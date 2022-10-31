5bc14582ccfd20709533250f6acce826f8bd9eff
Marco Wille <birdscur@outlook.dem>
2022-10-31 15:41:00 +0100

    v1.0

--- script.js
+++ script.js
@@ -1,17 +1,55 @@
 let currentPokemon;
 
 // Function to load pokemons
-async function loadPokemon() {
-    let url = "https://pokeapi.co/api/v2/pokemon/charmander";
-    let response = await fetch(url);
-    currentPokemon = await response.json();
-    console.log("Loaded Pokemon", currentPokemon);
 
-    renderPokemonInfo();
+function getId(id)
+{
+    return document.getElementById(id);
+}
+async function loadPokemon() 
+{
+   
+        
+    for (let i = 0; i < 151; i++)
+
+    {
+        let url = `https://pokeapi.co/api/v2/pokemon/${i + 1}`;
+        let response = await fetch(url);
+        currentPokemon = await response.json();
+        console.log("Loaded Pokemon", currentPokemon);
+    
+        //getId('pokemonName').innerHTML += `<div> ${currentPokemon["name"]} </div>`
+         renderPokemonInfo(i); 
+    } 
+    
 }
 
-function renderPokemonInfo() {
-    document.getElementById("pokemonName").innerHTML = currentPokemon["name"];
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  
+
+    function renderPokemonInfo(i)
+{
+    let pokemoneName =
+    
+    getId('cardBox').innerHTML += `<div id="pokemonCard${i}" class="pokemon-card">
+
+    <div id="pokemonId" class="poke-id">
+        # ${currentPokemon['id']}
+        <h1 id="pokemonName">${capitalizeFirstLetter(currentPokemon['name'])}</h1>
+    </div>
+
+    <img id="pokemonImage" src="${currentPokemon["sprites"]['other']['dream_world']['front_default']}">
+
+    <div id="pokemonType" class="pokemon-type">
+    
+    ${capitalizeFirstLetter(currentPokemon['types']['0']['type']['name'])}
+    </div>
+</div>`;
+    
 }
+
 
 
